fc50ccede689e70b12483e04c4e9753d2b82ba7b
noobandy <noobandy1364@gmail.com>
2014-05-14 17:30:34 +0530

    spring security

--- src/main/java/in/anandm/apps/template/interfaces/web/LoginController.java
+++ src/main/java/in/anandm/apps/template/interfaces/web/LoginController.java
@@ -36,31 +36,37 @@ public class LoginController {
 	public String login(@RequestParam(value="attempt",required=false) String attempt,Model model,
 			HttpServletRequest request){
 
-		if(attempt != null && attemptReason.contains(attempt)){
-			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-			if(authentication.isAuthenticated() && "success".equals(attempt)){
-				System.out.println(request.getRemoteAddr());
-				return "redirect:/";
-			}
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if(authentication != null && attempt != null && attemptReason.contains(attempt)){
 
-			if(!authentication.isAuthenticated() && "failed".equals(attempt)){
-				System.out.println(request.getRemoteAddr());
-				model.addAttribute("errorMsg", "Wrong user name or password.");
-				return "login/login";
-			}
+			boolean isAnonymous =  (authentication.getPrincipal() instanceof String && "anonymousUser".equals(authentication.getPrincipal()))? true : false;
+			
+			
+				if(authentication.isAuthenticated() && !isAnonymous && "success".equals(attempt)){
+					System.out.println(request.getRemoteAddr());
+					return "redirect:/";
+				}
 
-			if(!authentication.isAuthenticated() && "duplicateSession".equals(attempt)){
-				System.out.println(request.getRemoteAddr());
-				model.addAttribute("errorMsg", "concurrent session limit exceeded.");
-				return "login/login";
-			}
+				if(isAnonymous && "failed".equals(attempt)){
+					System.out.println(authentication.getPrincipal());
+					System.out.println(request.getRemoteAddr());
+					model.addAttribute("errorMsg", "Wrong user name or password.");
+					return "login/login";
+				}
 
-			if(!authentication.isAuthenticated() && "logOut".equals(attempt)){
-				System.out.println(request.getRemoteAddr());
-				model.addAttribute("successMsg", "You are successfully logged out.");
-				return "login/login";
-			}
+				if(isAnonymous && "duplicateSession".equals(attempt)){
+					System.out.println(authentication.getPrincipal());
+					System.out.println(request.getRemoteAddr());
+					model.addAttribute("errorMsg", "concurrent session limit exceeded.");
+					return "login/login";
+				}
 
+				if(isAnonymous && "logOut".equals(attempt)){
+					System.out.println(authentication.getPrincipal());
+					System.out.println(request.getRemoteAddr());
+					model.addAttribute("successMsg", "You are successfully logged out.");
+					return "login/login";
+				}
 		}
 		return "login/login";
 	}
